e1c1402c15bdb97e64ecc25b67666c7a936ba7b9
Karen Olivares <156404434+karenale2002@users.noreply.github.com>
2025-03-05 13:20:41 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Kathy: Helping you Elevate your Networking Skills`;
-export const MESSAGE_PLACEHOLDER: string = `I’m better known as Chatty Kathy, because I’m here to help you talk the talk in coffee chats, networking, and mentorship. Struggling with what to say? Need a confidence boost before reaching out? Want to practice a coffee chat before the real deal? I’ve got you covered! From crafting the perfect message to simulating real conversations, I’ll make sure you’re ready to build meaningful and lasting connections.`;
+export const MESSAGE_PLACEHOLDER: string = `edit message placeholder`;
 export const FOOTER_MESSAGE: string = `Customized AI by Karen Olivares @ UNC Chapel Hill`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `page title i need to change`;
+export const PAGE_TITLE: string = `Chatty Kathy AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
